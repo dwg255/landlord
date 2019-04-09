38f88056ca2cca3bea08b9499052338ed55e61bb
dwg255 <43668895+dwg255@users.noreply.github.com>
2019-04-09 15:14:25 +0800

    Update main.go
    add pid handle

--- main/main.go
+++ main/main.go
@@ -4,8 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/astaxie/beego/logs"
+	"io"
 	_ "landlord/router"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -32,5 +35,37 @@ func main() {
 	err = http.ListenAndServe(*addr, nil)
 	if err != nil {
 		logs.Error("ListenAndServe: err:%v", err)
+	}
+}
+
+func init() {
+	pidFileName := "pid"
+	fileInfo, err := os.Stat(pidFileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.Mkdir(pidFileName, os.ModePerm)
+			fileInfo, _ = os.Stat(pidFileName)
+		}
+	}
+	if fileInfo.IsDir() {
+		pid := os.Getpid()
+		pidFile, err := os.OpenFile(pidFileName+"/landlord.pid", os.O_RDWR|os.O_CREATE, 0766)
+		if err != nil {
+			logs.Error("open pidFile [%s] error :%v", pidFileName, err)
+			return
+		}
+		err = pidFile.Truncate(0)
+
+		_, err = io.WriteString(pidFile, strconv.Itoa(pid))
+		if err != nil {
+			logs.Error("write pid error :%v", err)
+		}
+
+		err = pidFile.Close()
+		if err != nil {
+			logs.Error("close pid file err: %v", err)
+		}
+	} else {
+		logs.Error("pidFile [%s] is exists and not dir", pidFileName)
 	}
 }
